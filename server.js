d3a9debb43906f8d08a76550bb5fbda0875cdc62
fady tadrous <fadytadrous.ft@gmail.com>
2021-07-18 11:59:02 +0200

    modified packages

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var history = require('connect-history-api-fallback');
 const servestatic = require('serve-static')
 const path = require('path')
 const express = require('express')
-const port = process.env.PORT || 8100;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(history());
 app.use(servestatic(path.join(path.resolve(), 'dist')));
-app.listen(port, () => {console.log("API server started on " + port);});
+app.listen(port, () => { console.log("API server started on " + port); });
